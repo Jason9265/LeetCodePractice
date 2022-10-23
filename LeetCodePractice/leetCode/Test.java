ccc59a0ec4e6774961172ff0cdcbf0f123ab35ca
Jason9265 <fjs971030@gmail.com>
2022-10-23 15:09:38 +1030

    add 2446,2447,LeetCode Contest

--- LeetCodePractice/leetCode/Test.java
+++ LeetCodePractice/leetCode/Test.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         Print p = new Print();
+        LeetCodeContest lcd = new LeetCodeContest();
 
         // TreeNode
         TreeBuilder tb = new TreeBuilder();
@@ -10,13 +11,12 @@ public class Test {
         TreeNode root = tb.iniTree(treeA);
 
         // array
-        int[] arr1 = new int[]{1, 2, 3, 4, 4};
+        int[] arr1 = new int[]{1,1,1,1,1};
         int[][] arra1 = new int[][]{{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
         int[][] arra2 = new int[][]{{0, 0}, {1, 0}, {2, 0}, {0, 1}, {0, 2}};
-        String[] stringArray = new String[]{"a"};
+        String[] stringArray1 = new String[]{"10:00", "11:00"};
+        String[] stringArray2 = new String[]{"01:20", "03:00"};
 
         // solution
-        LeetCode645 so = new LeetCode645();
-        p.printIntArray(so.findErrorNums(arr1));
     }
 }
